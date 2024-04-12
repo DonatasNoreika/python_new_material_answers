1aeb088af93d19a03b3a76e194b9c39e7844d6ca
DonatasNoreika <donoras@gmail.com>
2024-04-12 13:36:34 +0300

    Add files via upload

---
+++ Lesson 02/uzduotis2.py
@@ -0,0 +1,6 @@
+import datetime
+
+sentence = input("Enter sentence: ")
+
+print(sentence[::-1])
+print(sentence[::2])
---
+++ Lesson 02/uzduotis3.py
@@ -0,0 +1,13 @@
+import datetime
+
+a = float(input("Enter number a: "))
+b = float(input("Enter number b: "))
+
+print(f"a * b = {a * b}")
+print(f"a / b = {a / b}")
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+
+print(f"a // b = {a // b}")
+print(f"a % b = {a % b}")
+print(f"a ** b = {a ** b}")
